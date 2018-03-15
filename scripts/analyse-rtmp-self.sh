79403eb731692fe0eaa97527ebbfcc46fb08760f
heyu <hey@vmware.com>
2018-03-15 17:31:48 +0800

    keep compatible

--- scripts/analyse-rtmp-self.sh
+++ scripts/analyse-rtmp-self.sh
@@ -9,7 +9,7 @@ if [ $suc_times -gt 0 ]; then
     # address rtmp://play.jcloud.com/profiling/avatar, handshake_time 63, connection_time 334, firstItime 399, total_count 6, nonfluency_count 0, nonfluency_rate 0.00, sei_frame_count 4, e2e 202, e2relay 168, e2edge 42
     avg_handshake=$(cat $tmpfile | grep -Po 'handshake_time.*?(,|$)' | grep -Eo '[0-9]+' | awk '{sum+=$1} END {if (NR==0) print 9999999; else print sum/NR}')
     avg_connection=$(cat $tmpfile | grep -Po 'connection_time.*?(,|$)' | grep -Eo '[0-9]+' | awk '{sum+=$1} END {if (NR==0) print 9999999; else print sum/NR}')
-    avg_iframe=$(cat $tmpfile | grep -Po 'firstItime.*?(,|$)' | grep -Eo '[0-9]+' | awk '{sum+=$1} END {if (NR==0) print 9999999; else print sum/NR}')
+    avg_iframe=$(cat $tmpfile | grep -Po '(first_itime|firstItime).*?(,|$)' | grep -Eo '[0-9]+' | awk '{sum+=$1} END {if (NR==0) print 9999999; else print sum/NR}')
     avg_e2e=$(cat $tmpfile | grep -Po 'e2e.*?(,|$)' | grep -Eo '[0-9]+' | awk '{sum+=$1} END {if (NR==0) print 99999; else print sum/NR}')
     total_cnt=$(cat $tmpfile | grep -Po 'total_count.*?(,|$)' | grep -Eo '[0-9]+' | awk '{sum+=$1} END {print sum}')
     nonf_cnt=$(cat $tmpfile | grep -Po 'nonfluency_count.*?(,|$)' | grep -Eo '[0-9]+' | awk '{sum+=$1} END {print sum}')
