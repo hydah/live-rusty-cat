3eb0137d81aed45b365a57e0ff94a6a5a63e59d4
heyu <hey@vmware.com>
2018-03-22 15:06:08 +0800

    add extra waittime if socket read error

--- rtmp-play.cpp
+++ rtmp-play.cpp
@@ -8,7 +8,6 @@
 #include <sys/time.h>
 #include <sstream>
 #include <signal.h>
-#include <setjmp.h>
 #include "librtmp/srs_librtmp.h"
 #include "rtmp-tool.h"
 #include "argparse.hpp"
@@ -21,6 +20,7 @@ struct LiveRes{
     int64_t e2e;
     int64_t e2relay;
     int64_t e2edge;
+    int total_time;
     int runtime;
     int waittime;
     int sei_count;
@@ -30,7 +30,8 @@ struct LiveRes{
     LiveRes() {
         addr = "";
         e2e = e2relay = e2edge = 0;
-        runtime = waittime = handshake_time = connection_time = first_frame_time = 0;
+        runtime = total_time = waittime = 0;
+        handshake_time = connection_time = first_frame_time = 0;
         sei_count = 0;
     };
 };
@@ -39,13 +40,11 @@ struct Config {
     ArgumentParser parser;
     string addr;
     int total_time;
-    int count_interval;
     bool show_json;
     bool only_sum;
     bool debug;
     Config() {
         total_time = 10000;
-        count_interval = 1000;
         show_json = only_sum = debug = false;
     };
     void parse(int argc, const char **argv) {
@@ -58,17 +57,15 @@ struct Config {
             total_time = parser.get<int>("t");
         }
 
-        if (parser.valid("r")) {
-            count_interval = parser.get<int>("r");
-        }
     }
 } config;
-static jmp_buf buf;
+bool stop_playing = false;
 
 void sig_handler(int sig)
 {
     if (sig == SIGINT || sig == SIGTERM || sig == SIGALRM) {
-        longjmp(buf, 1);
+        log(WARNING, "got signal %d", sig);
+        stop_playing = true;
     }
 }
 
@@ -80,95 +77,103 @@ void run(LiveRes &live_res)
     int frame_count = 0;
     bool is_firstI = false;
     int64_t now_time, interval;
-    uint32_t last_ts = 0;
+    uint32_t last_ts = 0, timestamp = 0;;
     srs_rtmp_t rtmp = NULL;
 
     start_time = srs_utils_time_ms();
-    if (!setjmp(buf)) {
-        // startup socket for windows.
-        rtmp = srs_rtmp_create(live_res.addr.c_str());
-        // set 4s timeout
-        srs_rtmp_set_timeout(rtmp, 4000, 4000);
 
-        if (srs_rtmp_handshake(rtmp) != 0) {
-            log(ERROR, "simple handshake failed.");
-            goto rtmp_destroy;
+    // startup socket for windows.
+    rtmp = srs_rtmp_create(live_res.addr.c_str());
+    // set 4s timeout
+    srs_rtmp_set_timeout(rtmp, 4000, 4000);
+
+    if (srs_rtmp_handshake(rtmp) != 0) {
+        log(ERROR, "simple handshake failed.");
+        goto rtmp_destroy;
+    }
+    log(INFO, "simple handshake success");
+    live_res.handshake_time = srs_utils_time_ms() - start_time;
+
+    if (srs_rtmp_connect_app(rtmp) != 0) {
+        log(ERROR, "connect vhost/app failed.");
+        goto rtmp_destroy;
+    }
+    log(INFO, "connect vhost/app success");
+    live_res.connection_time = srs_utils_time_ms() - start_time;
+
+    if (srs_rtmp_play_stream(rtmp) != 0) {
+        log(ERROR, "play stream failed.");
+        goto rtmp_destroy;
+    }
+    log(INFO, "play stream success");
+
+    last_time = srs_utils_time_ms();
+    while(!stop_playing) {
+        int size;
+        char type;
+        char* data = NULL;
+        uint32_t _timestamp, pts;
+        uint32_t stream_id;
+        std::stringstream ss;
+        int ret = 0;
+
+        now_time = srs_utils_time_ms();
+
+        // if this func is non-block, it will be better.
+        if ((ret = srs_rtmp_read_packet(rtmp, &type, &_timestamp, &data, &size, &stream_id)) != 0) {
+            log(ERROR, "read packet error, errno is %d", ret);
+            break;
         }
-        log(INFO, "simple handshake success");
-        live_res.handshake_time = srs_utils_time_ms() - start_time;
+        timestamp = _timestamp;
 
-        if (srs_rtmp_connect_app(rtmp) != 0) {
-            log(ERROR, "connect vhost/app failed.");
-            goto rtmp_destroy;
+        char frame_type = srs_utils_flv_video_frame_type(data, size);
+        char avc_packet_type = srs_utils_flv_video_avc_packet_type(data, size);
+        now_time = srs_utils_time_ms();
+        if((avc_packet_type == SrsVideoAvcFrameTraitNALU)
+            && (frame_type == SrsVideoAvcFrameTypeKeyFrame || frame_type == SrsVideoAvcFrameTypeInterFrame)){
+            frame_count++;
+            int64_t tmp_time = srs_utils_time_ms();
+            int _wt = int(tmp_time - last_time) - int(timestamp - last_ts);
+            if (is_firstI && _wt > 0) {
+                live_res.waittime += _wt;
+                last_time = tmp_time;
+                last_ts = timestamp;
+            }
+            if(is_firstI == false && frame_type == SrsVideoAvcFrameTypeKeyFrame){
+                live_res.first_frame_time = now_time - start_time;
+                log(DEBUG, "play stream start and the first I frame arrive at %ld ms.", live_res.first_frame_time);
+                is_firstI = true;
+                last_time = tmp_time;
+                last_ts = timestamp;
+            }
         }
-        log(INFO, "connect vhost/app success");
-        live_res.connection_time = srs_utils_time_ms() - start_time;
 
-        if (srs_rtmp_play_stream(rtmp) != 0) {
-            log(ERROR, "play stream failed.");
-            goto rtmp_destroy;
+        if (false && srs_utils_is_metadata(type, data, size)) {
+            std::stringstream ms;
+            srs_print_metadata(data, size, ms);
+            log(INFO, "metadata is \n %s", ms.str().c_str());
         }
-        log(INFO, "play stream success");
 
-        last_time = srs_utils_time_ms();
-        for (;;) {
-            int size;
-            char type;
-            char* data = NULL;
-            uint32_t timestamp, pts;
-            uint32_t stream_id;
-            std::stringstream ss;
-            int ret = 0;
-
-            now_time = srs_utils_time_ms();
-
-            // if this func is non-block, it will be better.
-            if ((ret = srs_rtmp_read_packet(rtmp, &type, &timestamp, &data, &size, &stream_id)) != 0) {
-                log(ERROR, "read packet error, errno is %d", ret);
-                break;
-            }
-
-            char frame_type = srs_utils_flv_video_frame_type(data, size);
-            char avc_packet_type = srs_utils_flv_video_avc_packet_type(data, size);
-            now_time = srs_utils_time_ms();
-            if((avc_packet_type == SrsVideoAvcFrameTraitNALU)
-                && (frame_type == SrsVideoAvcFrameTypeKeyFrame || frame_type == SrsVideoAvcFrameTypeInterFrame)){
-                frame_count++;
-                int64_t tmp_time = srs_utils_time_ms();
-                int _wt = int(tmp_time - last_time) - int(timestamp - last_ts);
-                if (is_firstI && _wt > 0) {
-                    live_res.waittime += _wt;
-                    last_time = tmp_time;
-                    last_ts = timestamp;
-                }
-                if(is_firstI == false && frame_type == SrsVideoAvcFrameTypeKeyFrame){
-                    live_res.first_frame_time = now_time - start_time;
-                    log(DEBUG, "play stream start and the first I frame arrive at %ld ms.", live_res.first_frame_time);
-                    is_firstI = true;
-                    last_time = tmp_time;
-                    last_ts = timestamp;
-                }
-            }
-
-            if (false && srs_utils_is_metadata(type, data, size)) {
-                std::stringstream ms;
-                srs_print_metadata(data, size, ms);
-                log(INFO, "metadata is \n %s", ms.str().c_str());
-            }
-
-            if (srs_utils_is_sei_profiling(type, data, size)) {
-                srs_print_sei_profiling(data, size, ss, live_res.e2e, live_res.e2edge, live_res.e2relay);
-                live_res.sei_count++;
-                log(INFO, "node timestamp \n %s", ss.str().c_str());
-            }
-
-            delete data;
+        if (srs_utils_is_sei_profiling(type, data, size)) {
+            srs_print_sei_profiling(data, size, ss, live_res.e2e, live_res.e2edge, live_res.e2relay);
+            live_res.sei_count++;
+            log(INFO, "node timestamp \n %s", ss.str().c_str());
         }
+
+        delete data;
     }
 
     log(INFO, "play stream end");
 
 rtmp_destroy:
+    int64_t tmp_time = srs_utils_time_ms();
+    int _wt = int(tmp_time - last_time) - int(timestamp - last_ts);
+    if (is_firstI && _wt > 0) {
+        live_res.waittime += _wt;
+        last_time = tmp_time;
+        last_ts = timestamp;
+    }
+    live_res.runtime = tmp_time - start_time;
     srs_rtmp_destroy(rtmp);
     return;
 }
@@ -184,7 +189,8 @@ void print_result(LiveRes &live_res, bool print_json) {
     if (print_json == true) {
         cout << SRS_JOBJECT_START
              << SRS_JFIELD_STR("address", live_res.addr) << SRS_JFIELD_CONT
-             << SRS_JFIELD_ORG("duration", live_res.runtime) << SRS_JFIELD_CONT
+             << SRS_JFIELD_ORG("total_time", live_res.total_time) << SRS_JFIELD_CONT
+             << SRS_JFIELD_ORG("run_time", live_res.runtime) << SRS_JFIELD_CONT
              << SRS_JFIELD_ORG("handshake_time", live_res.handshake_time) << SRS_JFIELD_CONT
              << SRS_JFIELD_ORG("connection_time", live_res.connection_time) << SRS_JFIELD_CONT
              << SRS_JFIELD_ORG("first_itime", live_res.first_frame_time) << SRS_JFIELD_CONT
@@ -197,8 +203,8 @@ void print_result(LiveRes &live_res, bool print_json) {
         cout << endl;
 
     } else {
-        printf("address %s, handshake_time %d, connection_time %d, first_itime %d, duration %d, waiting_time %d, sei_frame_count %d",
-               live_res.addr.c_str(), live_res.handshake_time, live_res.connection_time, live_res.first_frame_time,
+        printf("address %s, handshake_time %d, total_time %d, connection_time %d, first_itime %d, run_time %d, waiting_time %d, sei_frame_count %d",
+               live_res.addr.c_str(), live_res.handshake_time, live_res.total_time, live_res.connection_time, live_res.first_frame_time,
                live_res.runtime, live_res.waittime, live_res.sei_count);
         printf(", e2e %d, e2relay %d, e2edge %d", avg_e2e, avg_e2relay, avg_e2edge);
         printf("\n");
@@ -209,7 +215,6 @@ int main(int argc, const char** argv)
 {
     config.parser.addArgument("-i", "--input_url", 1, false, "rtmp play url. like rtmp://127.0.0.1:1935/live/livestream");
     config.parser.addArgument("-t", "--total_time", 1, true, "the duration that this program will run, 10s if not specified");
-    config.parser.addArgument("-r", "--interval", 1, true, "the interval when checking frame rate, default is 1s");
     config.parser.addArgument("-d", "--debug", 0, true, "\tprint all the debug info");
     config.parser.addArgument("-s", "--only_summary", 0, true, "only print summary");
     config.parser.addArgument("-j", "--json", 0, true, "\tprint summary with json fomat");
@@ -218,7 +223,7 @@ int main(int argc, const char** argv)
 
     LiveRes live_res;
     live_res.addr = config.addr;
-    live_res.runtime = config.total_time;
+    live_res.total_time = config.total_time;
     if (config.only_sum) {
         init_loglevel(SUM);
     } else if (config.debug) {
@@ -229,7 +234,6 @@ int main(int argc, const char** argv)
     log(INFO, "srs(simple-rtmp-server) client librtmp library.");
     log(INFO, "version: %d.%d.%d", srs_version_major(), srs_version_minor(), srs_version_revision());
     log(INFO, "rtmp url: %s", live_res.addr.c_str());
-    log(INFO, "time_interval: %d", config.count_interval);
     log(INFO, "total_time: %d", config.total_time);
 
     signal(SIGALRM, sig_handler);
